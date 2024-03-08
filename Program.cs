e2025edaac2a55caf62aaf42d59cbb1b43792d59
Nompumelelo Hlongwane <61636340+Nompumelelo-np@users.noreply.github.com>
2024-03-08 19:02:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PalindromeWordChecker
+{
+    public class PalindromeWordExample
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string name = Console.ReadLine();
+            string reverse = string.Empty;
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverse += name[i];
+            }
+
+            if (name == reverse)
+            {
+                Console.WriteLine($"{name} is a Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} is not a Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
